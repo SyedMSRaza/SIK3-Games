3532cd18e28e582078db40793be098d3d8588586
ykevin98 <ykevin98@gmail.com>
2018-09-04 12:49:39 -0500

    Add Assets folder in

---
+++ Assets/Scripts/Spin_test_1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin_test_1 : MonoBehaviour {
+
+    public float speed;
+    public int rotate_speed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHoriz = Input.GetAxis("Horizontal");
+        float moveVerti = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHoriz, 0.0f, moveVerti);
+        rb.AddForce(movement * speed);
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3 (0, 10, 0) * Time.deltaTime, rotate_speed);
+    }
+}
---
+++ Assets/player_movements.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movements : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown("space"))
+        {
+            transform.Translate(Vector3.up * 50 * Time.deltaTime, Space.World);
+        }
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Spinner_test2")
+        {
+            // make the object react when touching another player/beyblade
+            transform.Translate(Vector3.back * 10 * Time.deltaTime, Space.World);
+        }
+    }
+}
